ab6602c1d1131ed02dd778dc58cc1f2140738908
Charles Tolley <charlestolley@gmail.com>
2018-01-19 10:33:28 -0700

    Added key expansion

--- aes.h
+++ aes.h
@@ -6,6 +6,8 @@
 #define NB 4
 #define WORD_SIZE 4
 
+typedef enum { AES128=4, AES192=6, AES256=8 } keylen_t;
+
 typedef union {
 	uint8_t bytes[WORD_SIZE];
 	uint32_t word;
@@ -15,7 +17,9 @@ typedef struct {
 	byteword_t cols[NB];
 } state_t;
 
-typedef state_t key_t;
+typedef struct {
+	byteword_t cols[AES256];
+} key_t;
 
 uint8_t ffmult(uint8_t a, uint8_t b);
 void subbytes(state_t * state);
@@ -24,5 +28,10 @@ void mixcolumns(state_t * state);
 void addroundkey(state_t * state, const key_t * key);
 
 void subword(byteword_t * word);
+void rotateword(byteword_t * word);
+
+/* key should be Nk*4 bytes long */
+/* round_keys should be Nk+7 key_t's long */
+void expand_keys(const uint8_t * key, key_t * round_keys, keylen_t Nk);
 
 #endif
